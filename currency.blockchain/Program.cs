b9fa1bcf75dd6da0840dfc71b3a0892d7efa1683
Milan Miljkovic <milan.lastname@outlook.com>
2019-08-24 19:47:13 -0400

    added api and bank sln

---
+++ currency.blockchain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace currency.blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
